a6b006b5d6d041dc9dc3141377e76cacf860a764
Jakob Keller <57402305+jakob-keller@users.noreply.github.com>
2023-02-25 19:42:32 +0100

    feat(datetime): implement constants `peprock.datetime.ONE_MICROSECOND`, `peprock.datetime.ONE_MILLISECOND`, `peprock.datetime.ONE_SECOND`, `peprock.datetime.ONE_MINUTE`, `peprock.datetime.ONE_HOUR`, `peprock.datetime.ONE_DAY`, `peprock.datetime.ONE_WEEK`

--- peprock/datetime.py
+++ peprock/datetime.py
@@ -2,6 +2,16 @@
 
 import datetime
 import functools
+import typing
+
+# constants
+ONE_MICROSECOND: typing.Final[datetime.timedelta] = datetime.timedelta(microseconds=1)
+ONE_MILLISECOND: typing.Final[datetime.timedelta] = datetime.timedelta(milliseconds=1)
+ONE_SECOND: typing.Final[datetime.timedelta] = datetime.timedelta(seconds=1)
+ONE_MINUTE: typing.Final[datetime.timedelta] = datetime.timedelta(minutes=1)
+ONE_HOUR: typing.Final[datetime.timedelta] = datetime.timedelta(hours=1)
+ONE_DAY: typing.Final[datetime.timedelta] = datetime.timedelta(days=1)
+ONE_WEEK: typing.Final[datetime.timedelta] = datetime.timedelta(weeks=1)
 
 
 # noinspection PyTypeChecker
@@ -73,6 +83,13 @@ def ensure_aware(
 
 
 __all__ = [
+    "ONE_MICROSECOND",
+    "ONE_MILLISECOND",
+    "ONE_SECOND",
+    "ONE_MINUTE",
+    "ONE_HOUR",
+    "ONE_DAY",
+    "ONE_WEEK",
     "is_naive",
     "is_aware",
     "EnsureAwareError",
--- tests/test_datetime.py
+++ tests/test_datetime.py
@@ -41,6 +41,22 @@ _ZONE_INFOS: typing.Final[tuple[datetime.tzinfo, ...]] = (
 
 
 @pytest.mark.parametrize(
+    ("constant", "expected"),
+    [
+        (peprock.datetime.ONE_MICROSECOND, datetime.timedelta(microseconds=1)),
+        (peprock.datetime.ONE_MILLISECOND, datetime.timedelta(milliseconds=1)),
+        (peprock.datetime.ONE_SECOND, datetime.timedelta(seconds=1)),
+        (peprock.datetime.ONE_MINUTE, datetime.timedelta(minutes=1)),
+        (peprock.datetime.ONE_HOUR, datetime.timedelta(hours=1)),
+        (peprock.datetime.ONE_DAY, datetime.timedelta(days=1)),
+        (peprock.datetime.ONE_WEEK, datetime.timedelta(weeks=1)),
+    ],
+)
+def test_constants(constant, expected):
+    assert constant == expected
+
+
+@pytest.mark.parametrize(
     ("arg", "is_naive"),
     [
         pytest.param(
